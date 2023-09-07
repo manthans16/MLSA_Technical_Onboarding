b7444a8a0c8b3e2944e2662862c8ef50bddecea1
Manthan Sharma <manthansharma1626@gmail.com>
2023-09-07 23:36:13 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
     const boxes = document.querySelectorAll(".box");
 
@@ -10,6 +8,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
         box.addEventListener("mouseleave", function () {
             box.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2)";
+        });
+    });
+
+    // Smooth scrolling for anchor links
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+    
+    anchorLinks.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth",
+                });
+            }
         });
     });
 });
